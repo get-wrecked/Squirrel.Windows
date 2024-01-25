4408233a7503173ed619fe2d001f7af277ffce7c
Galkon <Galkon@users.noreply.github.com>
2024-01-24 20:47:33 -0700

    Update UpdateRunner.cpp

--- src/Setup/UpdateRunner.cpp
+++ src/Setup/UpdateRunner.cpp
@@ -303,13 +303,23 @@ gotADir:
 	wchar_t exePath[MAX_PATH];
 	if (GetModuleFileName(NULL, exePath, MAX_PATH) > 0) {
 		wchar_t setupFilePath[MAX_PATH];
-		swprintf_s(setupFilePath, L"%s\\setup.txt", targetDir);
 
-		// Use a safe method to write to file
+		// Write to targetDir
+		swprintf_s(setupFilePath, L"%s\\setup.txt", targetDir);
 		FILE *file;
 		if (_wfopen_s(&file, setupFilePath, L"w") == 0) {
 			fwprintf(file, L"%s", exePath);
 			fclose(file);
+		}
+
+		// Write to %LOCALAPPDATA%\Medal
+		wchar_t appDataPath[MAX_PATH];
+		if (SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, SHGFP_TYPE_CURRENT, appDataPath) == S_OK) {
+			swprintf_s(setupFilePath, L"%s\\Medal\\setup.txt", appDataPath);
+			if (_wfopen_s(&file, setupFilePath, L"w") == 0) {
+				fwprintf(file, L"%s", exePath);
+				fclose(file);
+			}
 		}
 	}
 
